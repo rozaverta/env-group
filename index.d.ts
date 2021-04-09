95d7f10e6e998367a389406cd797a7c6466418cd
goshav <gosha@rozaverta.com>
2021-04-09 07:02:53 +0300

    updated index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
-declare module 'env-group' {
-	export default function EnvGroup <T = any>(prefix: string, alternative?: any, defaultValues?: Partial<T>): T;
-}
+declare function envGroup <T = any>(prefix: string, alternative?: any, defaultValues?: Partial<T>): T;
+declare function envGroup <T = any>(prefix: string, alternative: (key: string) => string, defaultValues?: Partial<T>): T;
+declare namespace envGroup {}
+export = envGroup;
